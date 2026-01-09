638bbb191134999857818f0d5d52295ddaa4edb8
Helio Abrahão <helioabrahaodepaula@gmail.com>
2026-01-08 21:43:55 -0300

    add 08/01

--- Códigos/MergeTwoSortedLists.py
+++ Códigos/MergeTwoSortedLists.py
@@ -4,5 +4,42 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        answer 
+    def mergeTwoLists(self, list1, list2):
+        p1 = list1
+        p2 = list2
+        
+        if not p1:
+            return p2
+        
+        if not p2:
+            return p1
+        
+        if p1.val <= p2.val:
+            newHead = p1
+            p1 = p1.next
+            
+        else:
+            newHead = p2
+            p2 = p2.next 
+            
+        answer = newHead
+        
+        while p1 is not None and p2 is not None:     
+            if p1.val <= p2.val:
+                newHead.next = p1
+                p1 = p1.next
+                newHead = newHead.next
+            else: 
+                newHead.next = p2
+                p2 = p2.next 
+                newHead = newHead.next
+                
+        if p1 is not None:
+            newHead.next = p1
+        else:
+            newHead.next = p2
+                
+        return answer
+           
+        
+        
